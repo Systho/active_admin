3be00237b29bdae3c3dcd63d1c0f1d89d5e6d327
Philippe Van Eerdenbrugghe <philippe.vaneerdenbrugghe@belighted.com>
2013-04-03 11:02:45 +0200

    do not use old reload for rails 4

--- lib/active_admin/reloader.rb
+++ lib/active_admin/reloader.rb
@@ -11,7 +11,9 @@ module ActiveAdmin
     #
     # @returns A concrete subclass of AbstractReloader
     def self.build(rails_app, active_admin_app, rails_version)
-      reloader_class = rails_version[0..2] == "3.2" ? Rails32Reloader : RailsLessThan31Reloader
+      reloader_class = RailsLessThan31Reloader
+      reloader_class = Rails32Reloader if rails_version[0..2] == "3.2"
+      reloader_class = Rails32Reloader if rails_version[0] == "4"
       reloader_class.new(rails_app, active_admin_app, rails_version)
     end
 
